75a9b0ad57e6843db6e293a33973b309f8e0eaf5
terror12 <32619913+terror12@users.noreply.github.com>
2018-02-08 19:39:33 -0500

    Update and rename Data Scraping to Historical_dtb_lineup_crt.py

---
+++ Workflow/Historical_dtb_lineup_crt.py
@@ -0,0 +1,13 @@
+# Python code to pull historical data from excel files into Python so that we can split create lineups based on a for loop
+# function that needs to be coded.
+
+# We will need to research ways to speed up for loops as much as possible
+
+# Once this code is complete it will generate lineups, along with their total salary and actual points scored
+ 
+# We may have this in a large dataframe or broken up into many different lineup objects 
+
+# We then can list them based on highest actual points scored to lowest.
+
+# We will need to add some type of check for Max and Min salary
+
