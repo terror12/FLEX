e5730c644c041e93bb18251037ec84f0b7040b10
acterror12 <acterror12@localhost.localdomain>
2019-05-04 11:09:09 -0400

    Update setup.py and add GNU License agreement

--- setup.py
+++ setup.py
@@ -1,1 +1,45 @@
-# Placeholder for packaging file
+#
+# Copyright(C) 2019 Adam Scerra
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.If not, see < https://www.gnu.org/licenses/>.
+
+import os
+from import walk
+from import join
+from import setup, find_packages
+from import __version__
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in walk(directory):
+        for filename in filenames:
+            paths.append((path, [join(path, filename)]))
+    return paths
+
+extra_files = package_files("flex")
+
+setup(
+    name="flex",
+    version=__version__,
+    author="Adam Scerra",
+    author_email="acterror12@gmail.com",
+    description="Package for all FLEXproject code",
+    url="https://github.com/terror12/FLEX.git",
+    packages=find_packages(),
+    data_files=extra_files,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
