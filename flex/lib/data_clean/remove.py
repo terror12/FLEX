f0ce8f2cceef68d238493cec6658b5c506764ffe
acterror12 <acterror12@localhost.localdomain>
2019-05-09 20:37:30 -0400

    Finished rm_col method and pytest for it!

--- flex/lib/data_clean/remove.py
+++ flex/lib/data_clean/remove.py
@@ -4,9 +4,38 @@ from flex.lib.connect.connect_to_sheets import  GoogleSheetsConnector
 class Remove:
 
     def columns(self, full_df_head):
+        """
+
+        :param full_df_head:
+        :return:
+        """
         # remove columns that I do not need
         df = full_df_head[['player', 'team', 'position', 'Actual_Points', 'FanDuel_Salary', 'Platform_AVG', 'STD']]
-        print(df)
+        print('\n %s' % df)
+        return df
+
+
+    def rm_cols(self, df):
+        """
+        Remove all unnecessary columns from Dataframe
+        :param df: The Dataframe
+        :return df: The Dataframe without uneeded columns
+        """
+
+        df = df[['player', 'team', 'position', 'Actual_Points', 'FanDuel_Salary', 'Platform_AVG', 'STD']]
+        print('\n %s' % df)
+        return df
+
+    def rm_FA(self, df):
+        """
+        Remove all Free Agents from Dataframe
+        :param df: The Dataframe
+        :return df: The Dataframe without FA's
+        """
+
+        # Removes all free agents
+        df = df[df.team != "FA"]
+
         return df
 
 # FLEX = GoogleSheetsConnector('1VZLj2gegd6RwDmE3UYprClaGsMe91TDrNw8fsC5ZbD4', 'A1:L537')
--- flex/tests/tier-0/testdataframe/test_cleanup.py
+++ flex/tests/tier-0/testdataframe/test_cleanup.py
@@ -1,6 +1,7 @@
 from glusto.core import Glusto as g
 from flex.lib.connect.connect_to_sheets import GoogleSheetsConnector
 from flex.lib.data_clean.fix_df import FixUpDf
+from flex.lib.data_clean.remove import Remove
 #from schema import Schema, And, Use, Optional
 import os.path
 from os import path
@@ -30,3 +31,38 @@ class TestCleanup:
         else:
             g.log.info('Headers DO NOT match')
             assert False
+
+    @pytest.mark.cols
+    def test_rm_cols(self, rawDataframe, print_logging):
+        """
+
+        :param rawDataframe:
+        :param print_logging:
+        :return:
+        """
+        FixUp_df = FixUpDf()
+        df = FixUp_df.fix_header(rawDataframe)
+
+        rm = Remove()
+        df = rm.rm_cols(df)
+
+        schema = ['player', 'team', 'position', 'Actual_Points', 'FanDuel_Salary', 'Platform_AVG', 'STD']
+
+        if df.columns.tolist() == schema:
+            g.log.info('Success Only necessary Columns Exist!!')
+            assert True
+        else:
+            g.log.info('Column Configuration is Incorrect!!')
+            assert False
+
+
+    @pytest.mark.FA
+    def test_rm_FA(self, rawDataframe, print_logging):
+        """
+
+        :param rawDataframe:
+        :param print_logging:
+        :return:
+        """
+        df = FixUpDf()
+        df = df.fix_header(rawDataframe)
