045a0331d0e5050fca22abf0dd68670cdd0fc0de
Adam Scerra <acterror12@localhost.localdomain>
2019-09-04 21:13:44 -0400

    Broke out a lot of test_build_lineup into createLineup function and created test_historic to start testing a range of granular STD on the current List.

--- flex/lib/generate_lineup/assemble_lineup.py
+++ flex/lib/generate_lineup/assemble_lineup.py
@@ -1,4 +1,5 @@
 from glusto.core import Glusto as g
+from flex.lib.generate_lineup.closest_to_num import ClosestToNum
 
 
 class Assemble():
@@ -51,4 +52,55 @@ class Assemble():
         if (lineup[-1] > 50000 and lineup[-1] < 65000):
             return True
         else:
+
             return False
+
+    def createLineup(self, QB, RB, WR, TE, FLX, DST, QB_STD, RB_STD, RB2_STD, WR_STD, WR2_STD, WR3_STD, TE_STD, FLX_STD, DST_STD):
+
+        g.log.info('Instantiate Closest_to_num object')
+        Closest_to_num = ClosestToNum()
+
+        RB2 = Closest_to_num.remove_closest(RB, RB_STD)
+        WR2 = Closest_to_num.remove_closest(WR, WR_STD)
+        WR3 = Closest_to_num.remove_closest(WR2, WR2_STD)
+
+        closest_QB_pos = Closest_to_num.find_closest_STD(QB, QB_STD)
+        closest_RB_pos = Closest_to_num.find_closest_STD(RB, RB_STD)
+        closest_RB2_pos = Closest_to_num.find_closest_STD(RB2, RB2_STD)
+        closest_WR_pos = Closest_to_num.find_closest_STD(WR, WR_STD)
+        closest_WR2_pos = Closest_to_num.find_closest_STD(WR2, WR2_STD)
+        closest_WR3_pos = Closest_to_num.find_closest_STD(WR3, WR3_STD)
+        closest_TE_pos = Closest_to_num.find_closest_STD(TE, TE_STD)
+        closest_FLX_pos = Closest_to_num.find_closest_STD(FLX, FLX_STD)
+        closest_DST_pos = Closest_to_num.find_closest_STD(DST, DST_STD)
+
+        QB = QB.values.tolist()
+        RB = RB.values.tolist()
+        RB2 = RB2.values.tolist()
+        WR = WR.values.tolist()
+        WR2 = WR2.values.tolist()
+        WR3 = WR3.values.tolist()
+        TE = TE.values.tolist()
+        FLX = FLX.values.tolist()
+        DST = DST.values.tolist()
+
+        # Create lineup object, this will be where the chosen players will be appended
+        lineup = []
+
+        lineup.append(QB[closest_QB_pos])
+        lineup.append(RB[closest_RB_pos])
+        lineup.append(RB2[closest_RB2_pos])
+        lineup.append(WR[closest_WR_pos])
+        lineup.append(WR2[closest_WR2_pos])
+        lineup.append(WR3[closest_WR3_pos])
+        lineup.append(TE[closest_TE_pos])
+        lineup.append(FLX[closest_FLX_pos])
+        lineup.append(DST[closest_DST_pos])
+
+        # Instantiate assemble object
+        assemble = Assemble()
+
+        lineup = assemble.sumTotalPoints(lineup)
+        lineup = assemble.sumTotalSalary(lineup)
+
+        return lineup
