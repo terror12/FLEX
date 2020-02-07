785febccb0b5b63a3c463a3a20e1c38ab0c6dfcd
terrro12 <acterror12@gmail.com>
2020-02-07 12:11:37 -0500

    add files to create for a full year

---
+++ flex/create_revised_data/2016.sh
@@ -0,0 +1,35 @@
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week1', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week1', 'Sheet_Name': 'Revised 2016 Week1 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week2', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week2', 'Sheet_Name': 'Revised 2016 Week2 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week3', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week3', 'Sheet_Name': 'Revised 2016 Week3 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week4', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week4', 'Sheet_Name': 'Revised 2016 Week4 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week5', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week5', 'Sheet_Name': 'Revised 2016 Week5 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week6', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week6', 'Sheet_Name': 'Revised 2016 Week6 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week7', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week7', 'Sheet_Name': 'Revised 2016 Week7 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week8', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week8', 'Sheet_Name': 'Revised 2016 Week8 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week9', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week9', 'Sheet_Name': 'Revised 2016 Week9 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week10', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week10', 'Sheet_Name': 'Revised 2016 Week10 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week11', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week11', 'Sheet_Name': 'Revised 2016 Week11 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week12', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week12', 'Sheet_Name': 'Revised 2016 Week12 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week13', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week13', 'Sheet_Name': 'Revised 2016 Week13 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week14', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week14', 'Sheet_Name': 'Revised 2016 Week14 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week15', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week15', 'Sheet_Name': 'Revised 2016 Week15 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week16', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week16', 'Sheet_Name': 'Revised 2016 Week16 STD', 'year': 16}" -m create_15_16_gsheet
+
+pytest -s -v -l --testdata="{'projections': '/home/pi/GameTime/FLEX/flex/Revised_Data/ffa_proj/Projections_2016_week17', 'FanDuel_Salaries': '/home/pi/GameTime/FLEX/flex/FanDuel_proj/FanDuel_Salaries_2016_week17', 'Sheet_Name': 'Revised 2016 Week17 STD', 'year': 16}" -m create_15_16_gsheet
+
+
