7bb908ced921672f193eddeba43b4a934fd880af
ascerra <ascerra@redhat.com>
2020-03-18 07:11:01 -0400

    Add template log files

--- flex/tests/tier-0/test_sheets_prereq/test_sheets_create.py
+++ flex/tests/tier-0/test_sheets_prereq/test_sheets_create.py
@@ -13,7 +13,7 @@ import pytest
 
 class TestSheetsCreate:
 
-    g.add_log(g.log, filename='./logs/sheets_create.log')
+    g.add_log(g.log, filename='./logs/sheets_create')
 
     @pytest.mark.create_sheet
     def test_create_sheet(self, print_logging):
